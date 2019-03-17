9a3c1686246c77e492b3706b32f50ff7e53a152c
suresh <subramanianganesan162@gmail.com>
2019-03-17 16:10:51 +0530

    92% Completed Project

---
+++ app/src/main/java/com/subu/stjosephs/tollpay/config/Constants.java
@@ -0,0 +1,12 @@
+package com.subu.stjosephs.tollpay.config;
+
+public class Constants {
+
+    public static final String M_ID = "BTxDXo84166162966776"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String CHANNEL_ID = "WEB"; //Paytm Channel Id, got it in paytm credentials
+    public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
+
+    public static final String WEBSITE = "WEBSTAGING";
+    public static final String CALLBACK_URL = "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp";
+
+}
